e2392e7e1db552fa547a55996c8798d9ca664827
vihangamanusha <vihangamanusha07@gmail.com>
2025-08-23 07:52:06 +0530

    update

--- Frontend/Admin/index.php
+++ Frontend/Admin/index.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "1234", "canteen");
+$mysqli = new mysqli("localhost", "root", "801@Vihanga", "smart_canteen");
 if ($mysqli->connect_error) die("Connection failed: " . $mysqli->connect_error);
 
 // Order counts by status
